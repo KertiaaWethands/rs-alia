5cf407f035398299767f988581a81d32f8b3c4d3
robbysobari1 <robbysobari.project@gmail.com>
2023-06-23 16:10:00 +0700

    last

--- resources/views/Artikel/artikel8.blade.php
+++ resources/views/Artikel/artikel8.blade.php
@@ -18,13 +18,20 @@
             <h1>4 Gejala Stunting yang Perlu Diwaspadai</h1>
             <p>Berikut ini adalah beberapa gejala stunting pada anak yang harus diwaspadai oleh para orangtua,
                 diantaranya adalah:</p>
-            <br>
             <ol>
                 <li>Pertumbuhan tulang pada anak yang tertunda</li>
                 <li>Berat badan rendah apabila dibandingkan dengan anak seusianya</li>
                 <li>Sang anak berbadan lebih pendek dari anak seusianya</li>
                 <li>Proporsi tubuh yang cenderung normal tapi tampak lebih muda/kecil untuk seusianya.</li>
             </ol>
+            <br>
+            <p>Upaya pencegahan untuk mengatasi stunting adalah hal yang utama. Para orangtua diharapkan bisa rutin
+                melakukan pemeriksaan kandungan ke fasilitas kesehatan terdekat, rutin mengkonsumsi Tablet Tambah Darah,
+                serta memenuhi asupan gizi, seperti protein hewani yang baik bagi tumbuh kembang janin.</p>
+            <p>Remaja putri aktif minum tablet tambah darah 1 tablet seminngu sekali. Pemberian ASI ekslusif pada bayi
+                selama 6 bulan. bayi di atas enam bulan diberikan konsumsi protein hewani dan tetap melanjutkan ASI.
+                Jangan lupa datang ke Posyandu setiap bulan untuk memantau pertumbuhan dan perkembangan serta imunisasi
+                balita.</p>
         </div>
         <div class="image-content">
             <img src="images/artikelpage7.png" alt="Gambar">
